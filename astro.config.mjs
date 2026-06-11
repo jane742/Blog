d484e865bca4c93f61b45c156d73c22d51cac198
jane742 <janeteslya@gmail.com>
2026-06-11 21:53:11 +0300

    fix: force install vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'; // или vercel, netlify и т.д.
+import vercel from '@astrojs/vercel';
 import db from '@astrojs/db';
 
 export default defineConfig({
   output: 'server', // Middleware работает только в SSR режиме
-  adapter: node({
-    mode: 'standalone',
-  }),
+adapter: vercel(),
   integrations: [db()],
 });
