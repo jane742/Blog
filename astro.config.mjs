fd010530b2650ca9e9ad9afece9c5c03fbd2ebba
jane742 <janeteslya@gmail.com>
2026-06-12 22:32:30 +0300

    hack: add postinstall script to fake astro entrypoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,4 @@ export default defineConfig({
   output: 'server', 
   adapter: vercel(),
   integrations: [db()],
-  vite: {
-    ssr: {
-      // Цей рядок каже Vite: "Обробляй пакети astro та db як чисті ES-модулі, 
-      // не намагайся конвертувати їх у CommonJS"
-      noExternal: ['astro', '@astrojs/db'],
-    }
-  }
 });
